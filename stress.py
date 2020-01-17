6ade1be657dde1b4c245178b62f07a6a081245ce
Sweep-D <jimmyduong95@gmail.com>
2020-01-17 17:22:23 +1100

    open chrome a bunch of times

---
+++ stress.py
@@ -0,0 +1,14 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+count = 0
+browser = webdriver.Chrome()
+url = input("Enter Browser URL:")
+
+while count < 100:
+    browser.get(url)
+    browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+    print("Currently at ", count)
